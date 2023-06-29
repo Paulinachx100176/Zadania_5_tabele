64521a68165a6e50d95a307a8f37278534501889
Paulinachx100176 <128227114+Paulinachx100176@users.noreply.github.com>
2023-06-29 15:09:44 +0200

    Add files via upload

---
+++ zadanie_1_tabele.cs
@@ -0,0 +1,5 @@
+﻿Rozpoczęto kompilację...
+1>------ Kompilacja rozpoczęta: Projekt: ConsoleApp8, Konfiguracja: Debug Any CPU ------
+1>  ConsoleApp8 -> C:\Users\48798\source\repos\ConsoleApp8\bin\Debug\ConsoleApp8.exe
+========== Kompilacja: sukces — 1, niepowodzenie — 0, aktualne — 0, pominięto — 0 ==========
+========= Kompilacja rozpoczęto o 3:05 PM i czas trwania wyniósł 00,217 s ===========
---
+++ zadanie_2_tabele.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę wierszy:");
+        int wiersze = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj liczbę kolumn:");
+        int kolumny = int.Parse(Console.ReadLine());
+
+        int[,] tablica = new int[wiersze, kolumny];
+
+        int wartosc = 1;
+        for (int i = 0; i < wiersze; i++)
+        {
+            for (int j = 0; j < kolumny; j++)
+            {
+                tablica[i, j] = wartosc;
+                wartosc++;
+            }
+        }
+
+        for (int i = 0; i < wiersze; i++)
+        {
+            for (int j = 0; j < kolumny; j++)
+            {
+                Console.Write(tablica[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
